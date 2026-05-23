6794fb313b2d1a20bcdbee5a02eb29b5faca92ea
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2026-05-22 22:53:19 -0700

    modified the html.lua snippets to comment out unused snippet types

--- nvim/luasnippets/html.lua
+++ nvim/luasnippets/html.lua
@@ -1,15 +1,15 @@
 local ls = require("luasnip")
 -- some shorthands...
 local s = ls.snippet
-local n = ls.snippet_node
+--local n = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
-local f = ls.function_node
+--local f = ls.function_node
 local c = ls.choice_node
-local d = ls.dynamic_node
+--local d = ls.dynamic_node
 
-local extras = require("luasnip.extras")
-local rep = extras.rep
+--local extras = require("luasnip.extras")
+--local rep = extras.rep
 local fmt = require("luasnip.extras.fmt").fmt
 
 -- Keymap for insert and selections of snippets
